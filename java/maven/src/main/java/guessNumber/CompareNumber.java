60e96de598fc381efb535a676ce673fae4e18e84
zhaopengn <zhaopengn@gamil.com>
2015-02-06 10:56:08 +0800

    should_return_0A0B_when_get_tips_given_inpout_is_5678_and_answer_is_1234

--- java/maven/src/main/java/guessNumber/CompareNumber.java
+++ java/maven/src/main/java/guessNumber/CompareNumber.java
@@ -8,6 +8,15 @@ public class CompareNumber {
         if (answer.equals(input)) {
             return "4A0B";
         }
+        int containsCount = 0;
+        for (int i = 0, n = input.length(); i < n; i++) {
+            if (answer.contains(String.valueOf(input.charAt(i)))) {
+                containsCount++;
+            }
+        }
+        if (containsCount == 0) {
+            return "0A0B";
+        }
         return null;
     }
 }
--- java/maven/src/test/java/CompareNumberTest.java
+++ java/maven/src/test/java/CompareNumberTest.java
@@ -8,7 +8,7 @@ import static org.fest.assertions.api.Assertions.assertThat;
  */
 public class CompareNumberTest {
     @Test
-    public void should_return_4A0Bwhen_get_tips_given_inpout_is_1234_and_answer_is_1234() {
+    public void should_return_4A0B_when_get_tips_given_inpout_is_1234_and_answer_is_1234() {
         //given
         String input = "1234";
         String answer = "1234";
@@ -19,5 +19,16 @@ public class CompareNumberTest {
 
         //then
         assertThat(result).isEqualTo("4A0B");
+    }
+
+    @Test
+    public void should_return_0A0B_when_get_tips_given_inpout_is_5678_and_answer_is_1234() {
+        String input = "5678";
+        String answer = "1234";
+
+        CompareNumber compareNumber = new CompareNumber();
+        String result = compareNumber.getTips(input, answer);
+
+        assertThat(result).isEqualTo("0A0B");
     }
 }
