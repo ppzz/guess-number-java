a87099e3cc75f948a11aeb06a28f9ef871dda558
zhaopengn <zhaopengn@gamil.com>
2015-02-06 11:02:22 +0800

    rewrite getTips()

--- java/maven/src/main/java/guessNumber/CompareNumber.java
+++ java/maven/src/main/java/guessNumber/CompareNumber.java
@@ -5,9 +5,16 @@ package guessNumber;
  */
 public class CompareNumber {
     public String getTips(String input, String answer) {
-        if (answer.equals(input)) {
+        int aCount = 0;
+        for (int i = 0, n = input.length(); i < n; i++) {
+            if (answer.charAt(i) == input.charAt(i)) {
+                aCount++;
+            }
+        }
+        if (aCount == 4) {
             return "4A0B";
         }
+
         int containsCount = 0;
         for (int i = 0, n = input.length(); i < n; i++) {
             if (answer.contains(String.valueOf(input.charAt(i)))) {
